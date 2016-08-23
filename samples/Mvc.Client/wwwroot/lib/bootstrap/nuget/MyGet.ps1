a65dabad80f15667237e0285ed444675a1d141ea
Sergei <sgitwork@gmail.com>
2016-08-23 14:25:04 +0300

    FX

--- samples/Mvc.Client/wwwroot/lib/bootstrap/nuget/MyGet.ps1
+++ samples/Mvc.Client/wwwroot/lib/bootstrap/nuget/MyGet.ps1
@@ -10,5 +10,5 @@ $env:SourcesPath
 $bsversion = ((Get-Content $env:SourcesPath\package.json) -join "`n" | ConvertFrom-Json).version
 
 # create packages
-& $nuget pack "nuget\bootstrap.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath -Version $bsversion
-& $nuget pack "nuget\bootstrap.less.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath -Version $bsversion
+& $nuget pack $env:SourcesPath + "nuget\bootstrap.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath -Version $bsversion
+& $nuget pack $env:SourcesPath + "nuget\bootstrap.less.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $env:SourcesPath -Version $bsversion
