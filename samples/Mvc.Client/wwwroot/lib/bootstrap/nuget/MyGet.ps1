ab11018d7fd67affda10338701bbefcaf5aae08b
Sergei <sgitwork@gmail.com>
2016-08-23 12:51:39 +0300

    fix bug

--- samples/Mvc.Client/wwwroot/lib/bootstrap/nuget/MyGet.ps1
+++ samples/Mvc.Client/wwwroot/lib/bootstrap/nuget/MyGet.ps1
@@ -1,7 +1,7 @@
 $nuget = $env:NuGet
 $sourcesPath = '..'
 # parse the version number out of package.json
-$bsversion = ((Get-Content $$sourcesPath\package.json) -join "`n" | ConvertFrom-Json).version
+$bsversion = ((Get-Content $sourcesPath\package.json) -join "`n" | ConvertFrom-Json).version
 
 # create packages
 & $nuget pack "nuget\bootstrap.nuspec" -Verbosity detailed -NonInteractive -NoPackageAnalysis -BasePath $sourcesPath -Version $bsversion
